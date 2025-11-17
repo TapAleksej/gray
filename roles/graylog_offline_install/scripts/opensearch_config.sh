a61cd454a66c7897850e04c95db1df92befe4934
Tapekhin Aleksey <gray@yandex.ru>
2025-11-17 16:39:10 +0300

    installed opesearh

--- roles/graylog_offline_install/scripts/opensearch_config.sh
+++ roles/graylog_offline_install/scripts/opensearch_config.sh
@@ -1,33 +1,44 @@
 #!/usr/bin/bash
-#set -x
+set -x
 
 filecnfg="/etc/opensearch/opensearch.yml"
 
+# --- Очистка demo-конфига OpenSearch Security ---
+sudo sed -i '/^######## Start OpenSearch Security Demo Configuration ########/,/^######## End OpenSearch Security Demo Configuration ########/d' "${filecnfg}"
+
+# Удаляем все security-настройки
+sudo sed -i '/^plugins\.security\./d' "${filecnfg}"
+
+# Базовые настройки
 sudo sed -i "s/^#\?cluster.name:.*/cluster.name: graylog/" "${filecnfg}"
-#sudo sed -i "s/^#\?node.name:.*/node.name: ${HOSTNAME}/" "${filecnfg}"
-sudo sed -i 's/^#\?node.name:.*/node.name: ${HOSTNAME}/' "${filecnfg}"
+sudo sed -i "s/^#\?node.name:.*/node.name: ${HOSTNAME}/" "${filecnfg}"
 sudo sed -i "s/^#\?network.host:.*/network.host: 0.0.0.0/" "${filecnfg}"
-#sudo sed -i "s/^#\?plugins.security.ssl.http.enabled: true*/plugins.security.ssl.http.enabled: false/" "${filecnfg}"
 
+# Полностью отключить security plugin
+if ! grep -q "^plugins.security.disabled" "${filecnfg}"; then
+  echo "plugins.security.disabled: true" | sudo tee -a "${filecnfg}" > /dev/null
+fi
 
+# Отключение transport (обходит bootstrap-check)
+if ! grep -q "^plugins.security.ssl.transport.enabled" "${filecnfg}"; then
+  echo "plugins.security.ssl.transport.enabled: false" | sudo tee -a "${filecnfg}" > /dev/null
+fi
 
-
+# Минимальные параметры кластера
 if ! grep -q "^discovery.type" "${filecnfg}"; then
-# Добавляем строки в конец (через tee, чтобы сработал sudo)
-  {
-    echo "discovery.type: single-node"
-    echo "action.auto_create_index: false"
-    echo "plugins.security.disabled: true"
-    echo "indices.query.bool.max_clause_count: 32768"
-  } | sudo tee -a "${filecnfg}" > /dev/null
+  echo "discovery.type: single-node" | sudo tee -a "${filecnfg}" > /dev/null
 fi
 
- sudo sysctl -w vm.max_map_count=262144
-
-if ! grep -q '^vm.max_map_count=262144' /etc/sysctl.conf; then
-        echo "vm.max_map_count=262144" | sudo tee -a /etc/sysctl.conf > /dev/null
+if ! grep -q "^action.auto_create_index" "${filecnfg}"; then
+  echo "action.auto_create_index: false" | sudo tee -a "${filecnfg}" > /dev/null
 fi
 
+# Системные параметры ядра
+sudo sysctl -w vm.max_map_count=262144
 
- sudo systemctl daemon-reload
-#cat "${filecnfg}"
+if ! grep -q "vm.max_map_count=262144" /etc/sysctl.conf; then
+  echo "vm.max_map_count=262144" | sudo tee -a /etc/sysctl.conf > /dev/null
+fi
+
+sudo systemctl daemon-reload
+sudo systemctl restart opensearch
